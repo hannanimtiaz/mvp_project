a30e6a3788a77bb55bfa65d91a72cc426cc51306
Sarah-Sajid <sarahsajidonly@gmail.com>
2020-12-09 14:15:44 +0500

    Retail Signup routers setup

--- routes/auth.js
+++ routes/auth.js
@@ -12,18 +12,22 @@ router.get('/admin/login', adminAuth.getLogin);
 /* Post admin login. */
 router.post('/admin/login', adminAuth.postLogin);
 
-
 /* GET retail user login page. */
 router.get('/retail/login', retailAuth.getLogin);
 
 /* Post retail user login. */
 router.post('/retail/login', retailAuth.postLogin);
 
+/* Get retail user signup page. */
+router.get('/retail/signup', retailAuth.getSignup);
+
 /* GET trade user login page. */
 router.get('/trade/login', tradeAuth.getLogin);
 
 /* Post trade user login. */
 router.post('/trade/login', tradeAuth.postLogin);
+
+
 
 
 module.exports = router;
