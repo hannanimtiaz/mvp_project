fc0eed4085ff6a1cf826f4a268d869bd801fc284
hannanimtiaz <m.hannan.imtiaz@gmail.com>
2020-12-10 17:57:36 +0500

    post signup trade

--- controllers/tradeUser/auth.js
+++ controllers/tradeUser/auth.js
@@ -5,6 +5,35 @@ exports.getLogin = function (req, res, next) {
     res.render('login', { type: 'Trade User' });
 }
 
+exports.postSignup = async function (req, res) {
+    const { gender, firstname, lastname, email, password, phone_no, postal_code, DOB, companyName, businessNumber, tradingNumber, address, city, businessType, companyRole, companySize } = req.body;
+
+    let isEmail = TradeModel.exists({ email: email })
+
+    if (isEmail) {
+        return res.json({
+            msg: 'User already exists'
+        })
+    }
+    else {
+        let trade = await TradeModels.create({
+            gender, firstname, lastname, email, password, phone_no, DOB, companyName, businessNumber, tradingNumber, address, city, businessType, companyRole, companySize
+        });
+        console.log('trade: ', trade);
+
+        if (trade) {
+            return res.json({
+                msg: 'User created'
+            });
+        }
+        else {
+            return res.json({
+                msg: 'User not created'
+            });
+        }
+    }
+}
+
 exports.postLogin = async function (req, res, next) {
     const { email, password } = req.body;
 
---
+++ models/tradeUser.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var tradeSchema = new Schema({
+    email: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    phone_no: String,
+    postal_Code: String,
+    DOB: String,
+    gender: {
+        type: String,
+        enum: ['male', 'female'],
+    }, companyName: String,
+    businessNumber: String,
+    tradingNumber: String,
+    address: String,
+    city: String,
+    businessType: String,
+    companyRole: String,
+    companySize: String
+
+});
+
+var Trade = mongoose.model('Trade',
+    tradeSchema);
+
+module.exports = Trade;
